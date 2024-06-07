995ec2d693d3837c0148e15b1229eec898f38995
Hazaldogan1825 <hazaldogan479@gmail.com>
2024-06-07 15:15:55 +0300

    Add files via upload

---
+++ arraylist_uyg/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arraylist_uyg
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArrayList liste = new ArrayList();
+            liste.Add("Bilişim");
+            liste.Add('b');
+            liste.Add(100);
+            liste.Remove('b');
+            foreach (object i in liste)
+            {
+                listBox1.Items.Add(i);
+            }
+            {
+
+            }
+        }
+    }
+}
